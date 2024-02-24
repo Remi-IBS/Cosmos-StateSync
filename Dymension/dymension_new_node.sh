92bb1009a46a602d15dc8bacc0528a2d0b2af81e
Rémi <109862559+Remi-IBS@users.noreply.github.com>
2024-02-24 16:07:27 +0100

    Update dymension_new_node.sh
    update gas-prices

--- Dymension/dymension_new_node.sh
+++ Dymension/dymension_new_node.sh
@@ -108,7 +108,7 @@ sleep 2
   s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_LISTEN_ADD}\"| ; \
   s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$SEEDS\"|" $DAEMON_HOME/config/config.toml
 
-  sed -E -i -s 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"20000000000adym\"/' $DAEMON_HOME/config/app.toml
+  sed -E -i -s 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"5000000000adym\"/' $DAEMON_HOME/config/app.toml
 
   $BINARYNAME tendermint unsafe-reset-all --home $DAEMON_HOME
 
